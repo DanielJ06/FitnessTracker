c23787b56043e86a0c7a574ffeb3858808a99804
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-02-01 17:26:48 -0300

    Dynamic recycleView view holders

--- app/src/main/java/com/danieljrodrigues/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/danieljrodrigues/fitnesstracker/MainActivity.java
@@ -10,7 +10,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,30 +34,41 @@ public class MainActivity extends AppCompatActivity {
 //            startActivity(intent);
 //        });
         rvMain = findViewById(R.id.rv_main);
+
+        List<MainItem> mainItems = new ArrayList<>();
+        mainItems.add(new MainItem(1, R.drawable.imc_icon, R.string.imc, R.color.purple_700));
+
         rvMain.setLayoutManager(new LinearLayoutManager(this));
-        MainAdapter adapter = new MainAdapter();
+        MainAdapter adapter = new MainAdapter(mainItems);
         rvMain.setAdapter(adapter);
     }
 
     private class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
 
+        private List<MainItem> mainItems;
+
+        public MainAdapter(List<MainItem> mainItems) {
+            this.mainItems = mainItems;
+        }
+
         @NonNull
         @Override
         public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             return new MainViewHolder(
-                LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.main_item, parent, false)
+                    LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.main_item, parent, false)
             );
         }
 
         @Override
         public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
-            holder.bind(position);
+            MainItem current = mainItems.get(position);
+            holder.bind(current);
         }
 
         @Override
         public int getItemCount() {
-            return 5;
+            return mainItems.size();
         }
     }
 
@@ -61,9 +77,14 @@ public class MainActivity extends AppCompatActivity {
             super(itemView);
         }
 
-        public void bind(int index) {
+        public void bind(MainItem mainItemIndex) {
             TextView title = itemView.findViewById(R.id.rv_title);
-            title.setText("IMC" + index);
+            ImageView icon = itemView.findViewById(R.id.rv_drawable);
+            LinearLayout container = (LinearLayout) itemView;
+
+            title.setText(mainItemIndex.getTextStringId());
+            icon.setImageResource(mainItemIndex.getDrawableId());
+            container.setBackgroundColor(mainItemIndex.getColor());
         }
     }
 }
